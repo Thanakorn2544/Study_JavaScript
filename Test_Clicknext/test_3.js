231ec95f65fb6185b5bdbd5b6811040a3ac8da64
Thanakorn2544 <thanakorn.sooksumran@gmail.com>
2022-09-02 22:07:46 +0700

    New commit

--- Test_Clicknext/test_3.js
+++ Test_Clicknext/test_3.js
@@ -1,15 +1,32 @@
 function tv_program_list(Array) {
     let result = "";
     for (let i = 0; i < Array.length; i++) {
-        if (i != Array.length-1) {
-            if (Array[i]-Array[i+1] == 1) {
-                result += Array[i] + "-" + Array[i+1];
+        if (i != Array.length) {
+            if (Array[i] == Array[i+1]-1) {
+                result += Array[i] + "-";
+                while (i < Array.length) {
+                    if (Array[i] == Array[i+1]-1) {
+                        i++;
+                    }
+                    else {
+                        result += Array[i];
+                        break;
+                    }
+                }
+            }
+            else {
+                result += Array[i];
             }
         }
+        if (i == Array.length-1) {
+            break;
+        }
+        result += ", ";
     }
-    return 0;
+    return result;
 }
 
-let Array = [1, 4, 6, 9, 10, 14, 16, 17]
+let Array = [1, 4, 6, 9, 10, 14, 16, 17, 18]
+let Array2 = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10]
 
-console.log(Array)
+console.log(tv_program_list(Array))
